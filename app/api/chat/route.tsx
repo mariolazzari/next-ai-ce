7cd2360e7dc525edf3bf9bad98f646d3f2023b3d
Mario Lazzari <mario.lazzari@gmail.com>
2025-08-24 10:16:09 +0200

    Done lesson 7 chat api

---
+++ app/api/chat/route.tsx
@@ -0,0 +1,18 @@
+import { openai } from "@ai-sdk/openai";
+import { convertToModelMessages, streamText, UIMessage } from "ai";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+    const result = streamText({
+      model: openai("gpt-4.1-nano"),
+      messages: convertToModelMessages(messages),
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.error("Error streaming response:", error);
+    return NextResponse.json("Internal Server Error", { status: 500 });
+  }
+}
